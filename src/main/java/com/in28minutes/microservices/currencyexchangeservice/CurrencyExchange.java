250bc815351ee3dd6f468c5a6ff1a3206cd0aeff
gaurav-b <sunymca619@gmail.com>
2022-11-10 19:22:42 +0530

    making use of in mem database h2

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -2,10 +2,20 @@ package com.in28minutes.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class CurrencyExchange {
 
+	@Id
 	private long id;
+	
+	@Column(name="currency_from")
 	private String from;
+	
+	@Column(name="currency_to")
 	private String to;
 	private BigDecimal conversionMultiple;
 	private String environment;
